c5f013de50c10e3193f10ca99309b1b1a938356b
marwanferreira <mferreirads04@gmail.com>
2025-03-24 17:48:08 +0100

    🚀 Add admin page + fix dependencies + restore ng serve

---
+++ src/app/checkin/checkin.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-checkin',
+  imports: [],
+  templateUrl: './checkin.component.html',
+  styleUrl: './checkin.component.css'
+})
+export class CheckinComponent {
+
+}
