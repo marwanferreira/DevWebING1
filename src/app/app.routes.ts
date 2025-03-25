83a393fe05d1c63d8423480b9374f8baca232694
marwanferreira <mferreirads04@gmail.com>
2025-03-25 16:59:25 +0100

    Mise à jour de l'application avec la navigation et le routage fonctionnels

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,25 @@ import { AccueilComponent } from './accueil/accueil.component';
 
 export const routes: Routes = [
   { path: '', component: AccueilComponent },
+
+  {
+    path: 'visualisation',
+    loadComponent: () =>
+      import('./modules/visualisation/visualisation.component').then(m => m.VisualisationComponent)
+  },
+  {
+    path: 'gestion',
+    loadComponent: () =>
+      import('./modules/gestion/gestion.component').then(m => m.GestionComponent)
+  },
   {
     path: 'information',
-    loadComponent: () => import('./modules/information/information.component').then(m => m.InformationComponent)
+    loadComponent: () =>
+      import('./modules/information/information.component').then(m => m.InformationComponent)
+  },
+  {
+    path: 'administration',
+    loadComponent: () =>
+      import('./modules/administration/administration.component').then(m => m.AdministrationComponent)
   },
 ];
---
+++ src/app/modules/gestion/gestion.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-gestion',
+  standalone: true,
+  imports: [CommonModule],
+  template: `<h2>Bienvenue dans le module de gestion</h2>`,
+  styleUrls: []
+})
+export class GestionComponent {}
