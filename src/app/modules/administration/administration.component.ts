ebaca66e6ef44565b80dedd7e10362f397861b76
marwanferreira <mferreirads04@gmail.com>
2025-03-25 21:36:31 +0100

    Finalize admin approval flow and add test@test.com as simple user

--- src/app/modules/administration/administration.component.ts
+++ src/app/modules/administration/administration.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { GestionCandidaturesComponent } from './gestion-candidatures.component';
 
 @Component({
   selector: 'app-administration',
   standalone: true,
-  imports: [CommonModule],
-  template: `<h2>Bienvenue dans le module d'administration</h2>`,
-  styleUrls: []
+  imports: [CommonModule, GestionCandidaturesComponent],
+  templateUrl: './administration.component.html',
+  styleUrls: ['./administration.component.css']
 })
 export class AdministrationComponent {}
