e30c77d04803e66d9e67db56ddf7042663aa410a
Jebbour Basma <basmajebb@gmail.com>
2025-04-10 22:40:28 +0200

    modification de statistiques

--- src/app/modules/statistiques/statistiques.component.ts
+++ src/app/modules/statistiques/statistiques.component.ts
@@ -14,15 +14,27 @@ export class StatistiquesComponent implements OnInit {
   data: any[] = [];
   totalUsage: number = 0;
   mostUsed: any[] = [];
+  inefficientObjects: any[] = [];
 
   constructor(private firestore: Firestore) {}
 
   async ngOnInit() {
+    const userType = localStorage.getItem('userType');
+    const now = new Date();
+  
     const colRef = collection(this.firestore, 'connected-objects');
     const snapshot = await getDocs(colRef);
     this.data = snapshot.docs.map(doc => doc.data());
-
+  
     this.totalUsage = this.data.reduce((sum, obj) => sum + (obj.usageCount || 0), 0);
     this.mostUsed = [...this.data].sort((a, b) => (b.usageCount || 0) - (a.usageCount || 0)).slice(0, 3);
+  
+    if (userType === 'admin' || userType === 'complexe') {
+      this.inefficientObjects = this.data.filter(obj => {
+        const last = new Date(obj.lastInteraction);
+        const days = (now.getTime() - last.getTime()) / (1000 * 60 * 60 * 24);
+        return obj.usageCount < 5 || days > 30;
+      });
+    }
   }
-}
+}  
